89e9a21c7bbc2ac424191692d0c0fb1f8fb0fcdb
Eivind Bøhn <eivind.bohn@gmail.com>
2020-12-18 17:50:23 +0100

    fix bootstrapping in replay buffer

--- stable_baselines/common/buffers.py
+++ stable_baselines/common/buffers.py
@@ -52,7 +52,7 @@ class ReplayBuffer(object):
         """
         return len(self) == self.buffer_size
 
-    def add(self, obs_t, action, reward, obs_tp1, done, *extra_data, **extra_data_kwargs):
+    def add(self, obs_t, action, reward, obs_tp1, done, *extra_data, bootstrap=None, **extra_data_kwargs):
         """
         add a new transition to the buffer
 
@@ -62,6 +62,8 @@ class ReplayBuffer(object):
         :param obs_tp1: (Union[np.ndarray, int]) the current observation
         :param done: (bool) is the episode done
         """
+        if bootstrap is not None:
+            done = not bootstrap
         data = (obs_t, action, reward, obs_tp1, done, *extra_data,
                 *[extra_data_kwargs[k] for k in sorted(extra_data_kwargs)])
 
