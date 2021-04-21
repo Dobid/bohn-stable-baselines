a20ac1794fb7e2ebe9c7f33114f964aa2fe63ecb
Eivind Bøhn <eivind.bohn@gmail.com>
2021-04-21 16:59:26 +0200

    add var mask to normalization and update every for use with n_envs = 1

--- stable_baselines/common/running_mean_std.py
+++ stable_baselines/common/running_mean_std.py
@@ -2,7 +2,7 @@ import numpy as np
 
 
 class RunningMeanStd(object):
-    def __init__(self, epsilon=1e-4, shape=(), mean_mask=None):
+    def __init__(self, epsilon=1e-4, shape=(), update_every=1, mean_mask=None, var_mask=None):
         """
         calulates the running mean and std of a data stream
         https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance#Parallel_algorithm
@@ -14,12 +14,24 @@ class RunningMeanStd(object):
         self.var = np.ones(shape, 'float64')
         self.count = epsilon
         self.mean_mask = mean_mask
+        self.var_mask = var_mask
+        self.update_every = update_every
+        self.buf = []
 
     def update(self, arr):
+        if len(arr.shape) > 1 and arr.shape[0] == 1:
+            self.buf.append(arr)
+            if len(self.buf) < self.update_every:
+                return
+            else:
+                arr = np.concatenate(self.buf)
+                self.buf = []
         batch_mean = np.mean(arr, axis=0)
         if self.mean_mask is not None:
             batch_mean[self.mean_mask] = 0
         batch_var = np.var(arr, axis=0)
+        if self.var_mask is not None:
+            batch_var[self.var_mask] = 1
         batch_count = arr.shape[0]
         self.update_from_moments(batch_mean, batch_var, batch_count)
 
